d08469cbb86f9f7f80f20d72b99edcec6734591c
theoriginalralsei <sibolinaofrancisinigo@Gmail.com>
2025-11-30 14:31:50 +0800

    Added a database here too so you can check if it's from a specific channel

--- cogs/count.py
+++ cogs/count.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import aiosqlite
+from connection import get_database
 
 class Count(commands.Cog):
     def __init__(self, bot):
@@ -8,11 +10,46 @@ class Count(commands.Cog):
         self.counter = 0
 
     @commands.Cog.listener()
-    async def on_message_send(self, message):
-        if int(message) == self.counter + 1 and message.channel.id == 1416249596757282859:
-            print(True)
-        elif int(message) != self.counter + 1 and message.channel.id == 1416249596757282859:
-            print(False)
+    async def on_message(self,ctx: discord.Interaction ,message):
+
+        connection = get_database()
+        cursor = connection.cursor()
+
+        cursor.execute("""
+                       SELECT counting_channel FROM server
+                       WHERE guild_id = ?
+                       """, (message.author.guild.id,))
+
+        result = cursor.fetchone() 
+        connection.close()
+        cursor.close()
+
+        if not result:
+            return  
+
+        channel = message.guild.get_channel(result)
+        channel_id = result[0]
+
+        if not channel:
+            return
+
+        if message.channel.id != channel_id:
+            return
+
+        try:
+            number = int(message.content)
+
+            if number == self.counter + 1 and message.channel == channel:
+                self.counter = number
+                await message.add_reaction("✅")
+            else:
+                await message.add_reaction("❌")
+                self.counter = 0
+                await ctx.send(f"{message.author.mention} Has failed to count... Just how")
+
+        except ValueError:
+            pass
+
 
 async def setup(bot):
     await bot.add_cog(Count(bot))
