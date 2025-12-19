1a609bacb8045be1967863a64437ae90210d62d0
Ralsei <sibolinaofrancisinigo@gmail.com>
2025-12-19 15:51:11 +0800

    Revert "Sorta fixed the groupings ( hopefully )"
    This reverts commit 4bcd2bebac3c8bc8f40b399c04cfa9c0b47a4ad7.

--- cogs/actions.py
+++ cogs/actions.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from discord import app_commands
 
 
-class Action(commands.GroupCog, name="action"):
+class Action(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
