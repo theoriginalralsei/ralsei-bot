030a6e221652919b78611b5b1c3d7272bc3476cd
theroiginalralsei <sibolinaofrancisinigo@gmail.com>
2026-01-24 07:28:16 +0800

    Add curremcy and EXP

--- db/setup.sql
+++ db/setup.sql
@@ -1,13 +1,17 @@
-CREATE TABLE IF NOT EXISTS server (
-  guild_id INTEGER PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS server(
+  guild_id INTEGER PRIMARY KEY,
   welcome_channel INTEGER,
   counting_channel INTEGER,
   log_channel INTEGER
 );
-
-CREATE TABLE IF NOT EXISTS count_state (
+CREATE TABLE IF NOT EXISTS count_state(
   guild_id INTEGER PRIMARY KEY,
   current_count INTEGER,
   best_count INTEGER,
   last_user_id INTEGER
+);
+CREATE TABLE IF NOT EXISTS USER(
+  user_id INTEGER PRIMARY KEY,
+  EXP INTEGER DEFAULT 0,
+  currency INTEGER DEFAULT 100
 );
